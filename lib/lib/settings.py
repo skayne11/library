3b26ad2de4f0954af40f46c833b771722fc08cac
Skayne <skayne.pro@gmail.com>
2023-04-03 16:28:49 +0200

    ajout livre et detail livre

--- lib/lib/settings.py
+++ lib/lib/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,17 +79,13 @@ WSGI_APPLICATION = 'lib.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'livre': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'livres',
-        'USER': 'postgres',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'library',
+        'USER': 'skayne',
         'PASSWORD': '#Skayne11',
         'HOST': 'localhost',
         'PORT': '5432',
-    }
+    },
 }
 
 
@@ -127,7 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRES = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
