69a42a413f6b833901268bb99390b954fe94f527
Skayne <skayne.pro@gmail.com>
2023-04-05 16:47:53 +0200

    livre --> drive modif livre et ajout film

--- lib/livre/views.py
+++ lib/livre/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from .models import Livre
 
 def livre_index(request):
-    livre = Livre.objects.all()
-    context = {
-        'livres': livre
-    }
-    return render(request, 'livre.html', context)
+    livres = Livre.objects.all()
+    if request.method == "GET":
+        titre = request.GET.get('recherche')
+        if titre is not None:
+            livres = Livre.objects.filter(titre__icontains=titre)
+    return render(request, 'livre.html', {'livres': livres})
+
 
 def livre_detail(request, pk):
     livre_det = Livre.objects.get(pk=pk)
